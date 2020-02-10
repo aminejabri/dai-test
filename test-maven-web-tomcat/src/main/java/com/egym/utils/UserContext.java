4ee76f8cb4290e173714c23beb8421f781f65527
Jourdain31 <remi.jourdain_17@yahoo.fr>
2020-02-10 14:29:13 +0100

    user context add getUSer metode

--- test-maven-web-tomcat/src/main/java/com/egym/utils/UserContext.java
+++ test-maven-web-tomcat/src/main/java/com/egym/utils/UserContext.java
@@ -1,10 +1,14 @@
 package com.egym.utils;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
+
+import com.egym.entity.User;
+import com.egym.repositories.UserRepository;
 
 /**
  * Classe qui founit les détails d'un utilisateur context
@@ -15,12 +19,24 @@ import org.springframework.stereotype.Component;
 @Component
 public class UserContext {
 
+	@Autowired
+	UserRepository userRepo;
+	
 	public UserDetails getUserDetails() {
 
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 
 		if (!(auth instanceof AnonymousAuthenticationToken)) {
 			return (UserDetails)auth.getPrincipal();
+		}
+		return null;
+	}
+	
+	public User getUser() {
+		
+		if (getUserDetails()!= null) {
+			
+			return userRepo.getUserByUsername( getUserDetails().getUsername());
 		}
 		return null;
 	}
